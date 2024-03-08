4130cd371342d0e447332573d3010bc101769cfa
sniffsec <sniffsec@proton.me>
2024-03-08 23:50:27 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,64 @@
 
     <!-- Inicio -->
     <div id="inicio">
-      <h1 class="titulo"><stars />Informático</h1>
+      <h1 class="titulo flex"><stars />Informático</h1>
       <img src="./assets/media/foto.png" alt="Foto de perfil" id="fotoPerfil">
       <button id="descargarCv">Descargar CV</button>
     </div>
 
     <!-- Sobre mí -->
     <div id="sobreMi">
-      <h1 class="titulo2"><stars />Sobre mí</h1>
-      <contentContainer>
-        <p class="bio">Soy un estudiante de informática al que le gusta aprender nuevas tecnologias de diseño y programación ya sea front-end o back-end.<br>
-          Estoy acostumbrado a adaptarme a diferentes metodologias de trabajo y tengo una gran capacidad de aprendizaje.</p>
-        <p style="color: white;">Mis puntos fuertes son el desarrollo web y el uso de GNU/Linux</p>
+      <h1 class="titulo2 flex"><stars />Sobre mí</h1>
+      <div class="flex max">
+        <contentContainer>
+          <p class="texto">Soy un estudiante de informática al que le gusta aprender nuevas tecnologias de diseño y programación ya sea front-end o back-end.
+            Estoy acostumbrado a adaptarme a diferentes metodologias de trabajo y tengo una gran capacidad de aprendizaje.</p>
+          <p class="texto">Mis puntos fuertes son el desarrollo web y el uso de GNU/Linux</p>
+          <p style="color: white;">Por otra parte, en mi tiempo libre me gusta hacer mis propios proyectos, escuchar música he ir al gimnasio.</p>
+        </contentContainer>
+        <contentContainer style="min-width: 12rem;">
+          <p class="texto">Nombre: Álex</p>
+          <p class="texto">Apellidos: Frías Angulo</p>
+          <p class="texto">Residencia: Bilbao</p>
+          <p style="color: white;">Veículo: Moto (A1)</p>
+        </contentContainer>
+      </div>
+    </div>
+
+    <!-- Titulaciones -->
+    <div id="titulaciones">
+      <h1 class="titulo2 flex"><stars />Titulaciones</h1>
+      <contentContainer style="flex-direction: row;">
+        <div class="estudios texto">
+          <p class="texto">Educacion Secundaria</p>
+          <p class="texto">Técnico en Preimpresión Digital</p>
+          <p style="color: white;">Técnico en ASIR</p>
+        </div>
+        <div class="estudios texto">
+          <p class="texto">(Centro Uribe-Kosta BHI)</p>
+          <p class="texto">(Centro Emilio Campuzano)</p>
+          <p style="color: white;">(Centro Almi)</p>
+        </div>
+        <div class="estudios texto">
+          <p class="texto">2018-2021</p>
+          <p class="texto">2021-2023</p>
+          <p style="color: white;">2023-cursando</p>
+        </div>
+      </contentContainer>
+      <h1 class="subtitulo flex">Idiomas</h1>
+      <contentContainer style="flex-direction: row;">
+        <div class="estudios texto">
+          <p class="texto">Ingles</p>
+          <p style="color: white;">euskara</p>
+        </div>
+        <div class="estudios texto">
+          <p class="texto">B1</p>
+          <p style="color: white;">B2</p>
+        </div>
+        <div class="estudios texto">
+          <p class="texto">(Sin título pero en proceso de obtencion del B2)</p>
+          <p style="color: white;">()Titulación de HABE</p>
+        </div>
       </contentContainer>
     </div>
 
--- src/components/container.vue
+++ src/components/container.vue
@@ -4,14 +4,19 @@
 
 <template>
     <div id="contentContainer">
-        <slot>/</slot>
+        <slot></slot>
     </div>
 </template>
 
 <style>
 #contentContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     border-radius: 0.5rem;
     padding: 1rem 2rem;
     background-color: rgba(175, 175, 255, 0.1);
+    margin: 0.5rem;
+    text-align: justify;
 }
 </style>
