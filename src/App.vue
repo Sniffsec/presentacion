ba7ea4c6c61e844fe8d30d8bc8959265f641c2e8
sniffsec <sniffsec@proton.me>
2024-03-08 18:36:50 +0100

    primera suboda

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <navVar />
+  </div>
+</template>
+
+<script setup>
+import navVar from "./components/nav-var.vue"
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/nav-var.vue
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<template>
+    <div id="nav-var">
+        <ul>
+            <li>Inicio</li>
+            <li>Sobre mí</li>
+            <li>Titulaciones</li>
+            <li>Conocimientos</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss">
+#nav-var {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 5rem;
+}
+
+ul {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    list-style: none;
+    background-color: blue;
+    width: 40rem;
+    height: 3rem;
+    border-radius: 1rem;
+    box-shadow: 6px 6px 0px rgb(31, 240, 240);
+}
+
+li {
+    color: white;
+    font-weight: bold;
+    text-shadow: 3px 3px 15px rgb(0, 0, 0);
+}
+</style>
