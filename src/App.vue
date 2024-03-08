9e54e105bcd2b22f546eeb8170e1c75d4c4eaa77
sniffsec <sniffsec@proton.me>
2024-03-08 21:53:01 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,19 @@
 
     <!-- Inicio -->
     <div id="inicio">
-      
-        <h1 class="titulo"><stars />Informático</h1>
-        <img src="./assets/media/foto.png" alt="Foto de perfil" id="fotoPerfil">
-        <button id="descargarCv">Descargar CV</button>
+      <h1 class="titulo"><stars />Informático</h1>
+      <img src="./assets/media/foto.png" alt="Foto de perfil" id="fotoPerfil">
+      <button id="descargarCv">Descargar CV</button>
     </div>
 
     <!-- Sobre mí -->
     <div id="sobreMi">
-
+      <h1 class="titulo2"><stars />Sobre mí</h1>
+      <contentContainer>
+        <p class="bio">Soy un estudiante de informática al que le gusta aprender nuevas tecnologias de diseño y programación ya sea front-end o back-end.<br>
+          Estoy acostumbrado a adaptarme a diferentes metodologias de trabajo y tengo una gran capacidad de aprendizaje.</p>
+        <p style="color: white;">Mis puntos fuertes son el desarrollo web y el uso de GNU/Linux</p>
+      </contentContainer>
     </div>
 
   </div>
@@ -23,6 +27,7 @@
 <script setup>
 import navVar from "./components/nav-var.vue"
 import stars from "./components/stars.vue"
+import contentContainer from "./components/container.vue"
 </script>
 
 <style lang="scss">
---
+++ src/components/container.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+    <div id="contentContainer">
+        <slot>/</slot>
+    </div>
+</template>
+
+<style>
+#contentContainer {
+    border-radius: 0.5rem;
+    padding: 1rem 2rem;
+    background-color: rgba(175, 175, 255, 0.1);
+}
+</style>
