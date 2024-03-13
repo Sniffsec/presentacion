7425af0c684f0d836f716356a97560ecae01c5ce
sniffsec <sniffsec@proton.me>
2024-03-13 21:56:16 +0100

    update

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -1,4 +1,5 @@
 <script>
+import copy from "../assets/media/copy.svg"
 
 export default {
     data() {
@@ -6,7 +7,8 @@ export default {
             title: "Contacto",
             mail: "sniffsec@proton.me",
             github: "Sniffsec",
-            linkedin: "Álex Frías Angulo"
+            linkedin: "Álex Frías Angulo",
+            copy: copy
         }
     }
 }
@@ -16,13 +18,26 @@ export default {
 <template>
     <section class="all-screen centered column">
         <h2 class="blue-green-color-text centered big">{{ title }}</h2>
-        <div class="bordered-and-grey standard-padding centered wrap small-max">
-            <h2 class="text-without-margin bordered-and-purple-gradient min-padding centered min-column-margin">Mail</h2>
-                <label class="text-without-margin flex-wrap">{{ mail }}</label>
-            <h2 class="text-without-margin bordered-and-purple-gradient min-padding centered min-column-margin">Github</h2>
-                <label class="text-without-margin flex-wrap">{{ github }}</label>
-            <h2 class="text-without-margin bordered-and-purple-gradient min-padding centered min-column-margin">Linkedin</h2>
-                <label class="text-without-margin flex-wrap">{{ linkedin }}</label>
+        <div class="bordered-and-grey standard-padding small-max">
+            
+            <div class="flex-alighned">
+                <h3 class="text-without-margin bordered-and-purple-gradient min-padding centered min-column-margin">Mail</h3>
+                <label class="text-without-margin">{{ mail }}</label>
+                <img :src="copy" alt="copy" class="copy">
+            </div>
+
+            <div class="flex-alighned">
+                <h3 class="text-without-margin bordered-and-purple-gradient min-padding centered min-column-margin">Linkedin</h3>
+                <label class="text-without-margin">{{ linkedin }}</label>
+                <img :src="copy" alt="copy" class="copy">
+            </div>
+
+            <div class="flex-alighned">
+                <h3 class="text-without-margin bordered-and-purple-gradient min-padding centered min-column-margin">Github</h3>
+                <label class="text-without-margin">{{ github }}</label>
+                <img :src="copy" alt="copy" class="copy">
+            </div>
+            
         </div>
     </section>    
 </template>
--- src/components/redes.vue
+++ src/components/redes.vue
@@ -1,73 +1,61 @@
+<script>
+import cisco from "../assets/logos/cisco.svg"
+import linux from "../assets/logos/linux.svg"
+import windows from "../assets/logos/windows.svg"
+import mysql from "../assets/logos/mysql.svg"
+import mongodb from "../assets/logos/mongo.svg"
+import oracle from "../assets/logos/oracle.svg"
+
+export default {
+    data() {
+        return {
+            title1: "Redes",
+            text1: "En el ambito de las redes y sistemas tengo buen manejo, sobre todo en sistemas ya que tengo una buena base en GNU/Linux. Y en redes he recibido una buena formacion de Cisco.",
+            logo1: cisco,
+            logo2: linux,
+            logo3: windows,
+
+            title2: "Database",
+            text2: "Mysql es la base de datos con la que mas estoy familiarizado pero tambien se usar oracle. Tambien me va bien con MongoDb ya que  utiliza Js, lenguaje el cual estoy desarroyando mucho.",
+            logo4: mysql,
+            logo5: mongodb,
+            logo6: oracle
+        }
+    }
+}
+</script>
+
 <template>
-    <section id="redes">
-        <h2 class="titulo">Redes</h2>
-        <div id="contRedes">
-            <p class="texto textoRedDb">En el ambito de las redes y sistemas tengo buen manejo, sobre todo en sistemas ya que tengo una buena base en GNU/Linux. Y en redes he recibido una buena formacion de Cisco.</p>
-            <label class="tecnologias" for=""><img class="logos" src="../assets/logos/cisco.svg" alt="Cisco">Redes con Cisco</label>
-        </div>
-        
-        <h2 class="titulo">Database</h2>
-        <div id="contDb">
-            <p class="texto textoRedDb">Mysql es la base de datos con la que mas estoy familiarizado pero tambien se usar oracle. Tambien me va bien con MongoDb ya que  utiliza Js, lenguaje el cual estoy desarroyando mucho.</p>
-            <div id="contTecnologias">
-                <label class="tecnologias" for=""><img class="logos" src="../assets/logos/mongo.svg" alt="MongoDb">MongoDb</label>
-                <label class="tecnologias" for=""><img class="logos" src="../assets/logos/mysql.svg" alt="MySQL">MySQL</label>
-                <label class="tecnologias" for=""><img class="logos" src="../assets/logos/oracle.svg" alt="MongoDb">Oracle</label>
-            </div>
-        </div>
+    <section class="all-screen flex-around">
+        <article class="bordered-and-grey standard-padding small-max" id="article-1-of-networks">
+            <h2 class="blue-green-color-text centered big">{{ title1 }}</h2>
+            <p class="standard-text">{{ text1 }}</p>
+            <picture class="flex-around left-right-min-margin">
+                <label class="centered column standard-text"><img class="logos" :src="logo1">Cisco</label>
+                <label class="centered column standard-text"><img class="logos" :src="logo2">Linux</label>
+                <label class="centered column standard-text"><img class="logos" :src="logo3">Windows</label>
+            </picture>
+        </article>
+        <article class="bordered-and-grey standard-padding small-max" id="article-2-of-networks">
+            <h2 class="blue-green-color-text centered big">{{ title2 }}</h2>
+            <p class="standard-text">{{ text2 }}</p>
+            <picture class="flex-around left-right-min-margin">
+                <label class="centered column standard-text"><img class="logos" :src="logo4">MySQL</label>
+                <label class="centered column standard-text"><img class="logos" :src="logo5">MongoDB</label>
+                <label class="centered column standard-text"><img class="logos" :src="logo6">Oracle</label>
+            </picture>
+        </article>
     </section>
 </template>
 
 <style>
 
-#redes {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
+#article-1-of-networks {
+    margin-left: 5%;
 }
 
-#contRedes {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    width: 30rem;
-    padding: 2rem 3rem;
-    border-radius: 0.5rem;
-    background-color: rgba(175, 175, 255, 0.1);
-    margin-bottom: 7rem;
-}
-
-#contDb {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    width: 30rem;
-    padding: 2rem 3rem;
-    border-radius: 0.5rem;
-    background-color: rgba(175, 175, 255, 0.1);
-    margin-bottom: 7rem;
-}
-
-.textoRedDb {
-    font-weight: bold;
-    text-align: justify;
-}
-
-#contTecnologias {
-    display: flex;
-    justify-content: center;
+#article-2-of-networks {
+    margin-right: 5%;
 }
 
 </style>
-
-<script>
-
-</script>
